217769f3ce0188256aa9833acddfe2ffa003a638
Mike <mifear9@gmail.com>
2017-11-06 23:06:19 -0800

    Updated Comic Book Detail view to use the layout page.

--- ComicBookGallery/Controllers/ComicBooksController.cs
+++ ComicBookGallery/Controllers/ComicBooksController.cs
@@ -11,7 +11,7 @@ namespace ComicBookGallery.Controllers
     public ActionResult Detail()
     {
 
-      ViewBag.SeriesTitle = "the Amazing Spider-Man";
+      ViewBag.SeriesTitle = "The Amazing Spider-Man";
       ViewBag.IssueNumber = 700;
       ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
       ViewBag.Artists = new string[]
